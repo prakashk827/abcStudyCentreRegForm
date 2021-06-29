0b07e05691f86e45d72b8a6a6c614b8fc3babb6b
Prakash Koppal <prakashk827@gmail.com>
2021-06-29 07:27:27 +0530

    Added Sl.No, Remark coloumn in excel sheet

--- admin/export/exportFile.php
+++ admin/export/exportFile.php
@@ -30,33 +30,39 @@ $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
     $output .= '
    <table class="table">
-                     
+                         <th class="header" >Sl No</th> 
+                         <th class="header">Application Date</th> 
                          <th class="header" >Student Name</th>  
                          <th class="header" >School / College Name </th> 
                          <th class="header">Branch</th>
                          <th class="header">Combination</th>
-                         <th class="header">Join Date</th>
                          <th class="header" >Mother Name</th>  
                          <th class="header" >Father Name</th>  
-                         <th class="header">Father Mobile Number</th>
-                         <th class="header">Mother Mobile Number</th>
+                         <th class="header">Father Number</th>
+                         <th class="header">Mother Number</th>
                          <th class="header">WhatsApp</th>
+                         <th class="header">Remark&nbsp;&nbsp;</th>
+                        
 
 
                     </tr>
   ';
+    $count=1;
     while ($row = mysqli_fetch_array($result)) {
         $output .= '<tr>  
+                         <td class="body" style="text-align:center">' . $count++ . '</td> 
+                         <td class="body" style="text-align:center">' . $row["date"] . '</td> 
                          <td class="body">' . $row["studentName"] . '</td> 
                          <td class="body">' . $row["institute"] . '</td>
                          <td class="body">' . $row["branch"] . '</td>  
                          <td class="body">' . $row["combination"] . '</td> 
-                         <td class="body">' . $row["doj"] . '</td> 
                          <td class="body">' . $row["motherName"] . '</td> 
                          <td class="body">' . $row["fatherName"] . '</td>
-                         <td class="body">' . $row["motherMobile"] . '</td>
-                         <td class="body">' . $row["fatherMobile"] . '</td>
-                         <td class="body">' . $row["whatsApp"] . '</td>
+                         <td class="body" style="text-align:center">' . $row["motherMobile"] . '</td>
+                         <td class="body" style="text-align:center">' . $row["fatherMobile"] . '</td>
+                         <td class="body" style="text-align:center">' . $row["whatsApp"] . '</td>
+                         <td class="body"> </td>
+                        
 
                         
                             
